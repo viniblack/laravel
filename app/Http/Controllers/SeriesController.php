e6f0b2511b504575da455b96bfa146b8a4c8cf65
Vini Black <viniibl4ck@gmail.com>
2022-01-04 16:52:41 -0300

    criando validacao quando cadastra serie

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SeriesFormRequest;
 use App\Serie;
 use Illuminate\Http\Request;
 
@@ -21,8 +22,9 @@ class SeriesController extends Controller
     return view('series.create');
   }
 
-  public function store(Request $request)
+  public function store(SeriesFormRequest $request)
   {
+
     $serie = Serie::create($request->all());
     $request->session()->flash('mensagem', "Série {$serie->id} criada com sucesso {$serie->nome}");
 
